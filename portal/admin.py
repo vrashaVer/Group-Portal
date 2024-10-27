f50ed97ada6f627ab8b79dfa9f2cc782bd8a8e91
vrashaVer <da.programs.vr@gmail.com>
2024-10-27 14:56:21 +0200

    create models for main page
    create Announcement, AnnouncementPhoto, Comment, Like, Poll, Choice, Vote

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from portal.models import Announcement,AnnouncementPhoto,Comment,Like,Poll,Choice,Vote
 
-# Register your models here.
+admin.site.register(Announcement)
+admin.site.register(AnnouncementPhoto)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Poll)
+admin.site.register(Choice)
+admin.site.register(Vote)
+
+
